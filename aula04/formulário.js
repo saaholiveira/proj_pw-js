e24d1bb679a0e400b0ee46c87c546f77cf392ef1
Sarah oleira <osarah381@gmail.com>
2024-09-16 18:11:47 -0300

    algumas correções

--- aula04/formulário.js
+++ aula04/formulário.js
@@ -9,30 +9,31 @@ function adicionarLinha() {
       //Se for preenchido vazio 
       if(nome === '' || idade === '' || admissao === '') {
             alert('Preencha todos os campos!')
-      }
+      } else {
+            const tabela = document.getElementById("tabeladados").getElementsByTagName('tbody')[0];
+
+            //Inserindo uma nova linha
+            const novaLinha = tabela.insertRow();
+            //Inserindo valores de linha
+            const celulaid = novaLinha.insertCell(0);
+            const cellNome = novaLinha.insertCell(1);
+            const cellIdade = novaLinha.insertCell(2);
+            const cellAdmissao = novaLinha.insertCell(3);
+            const cellDemissao = novaLinha.insertCell(4);
+      
+            // Inserindo os valores dentro das células
+            celulaid.innerHTML = proximoId;
+            cellNome.innerHTML = nome;
+            cellIdade.innerHTML = idade;
+            cellAdmissao.innerHTML = admissao;
+            cellDemissao.innerHTML = demissao;
+      
+            proximoId++
+      
+            //Limpar formulário
+            document.getElementById('linhaForm').requestFullscreen();}
       // Cria uma linha da tabela se não existir
-      const tabela = document.getElementById("tabeladados").getElementsByTagName('tbody')[0];
-
-      //Inserindo uma nova linha
-      const novaLinha = tabela.insertRow();
-      //Inserindo valores de linha
-      const celulaid = novaLinha.insertCell(0);
-      const cellNome = novaLinha.insertCell(1);
-      const cellIdade = novaLinha.insertCell(2);
-      const cellAdmissao = novaLinha.insertCell(3);
-      const cellDemissao = novaLinha.insertCell(4);
-
-      // Inserindo os valores dentro das células
-      celulaid.innerHTML = proximoId;
-      cellNome.innerHTML = nome;
-      cellIdade.innerHTML = idade;
-      cellAdmissao.innerHTML = admissao;
-      cellDemissao.innerHTML = demissao;
-
-      proximoId++
-
-      //Limpar formulário
-      document.getElementById('linhaForm').requestFullscreen();
+      
 
       
 }
